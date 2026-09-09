0549be1bddff4e36c614f26a8ce44c83d2fce374
MOHAMMED SALIM ALAZZAME <169025034+MOHAMMEDQATA@users.noreply.github.com>
2026-09-09 04:12:40 +0400

    Use environment variable for port configuration

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def webhook():
     print(data)
 
     return "EVENT_RECEIVED", 200
-
+    import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
